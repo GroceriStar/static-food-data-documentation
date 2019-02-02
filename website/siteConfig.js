e15b986ea198d440bc3517f2b8db8bac9d12799d
Vadim <vadim.putrov@gmail.com>
2019-02-02 14:09:22 +0200

    change projectName

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
      baseUrl: '/static-food-data-documentation/',
 
   // Used for publishing and more
-  projectName: 'groceristar-fetch',
+  projectName: 'static-food-data-documentation',
   organizationName: 'groceristar',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
