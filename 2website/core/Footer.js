8f97fa3fbd262c1a0963434a7877504048f42dbb
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-09 07:22:53 +0200

    bad changes, but let

--- 2website/core/Footer.js
+++ 2website/core/Footer.js
@@ -10,18 +10,16 @@ const React = require('react');
 class Footer extends React.Component {
   docUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    const docsUrl = this.props.config.docsUrl;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    return `${baseUrl}${docsPart}${langPart}${doc}`;
+    return baseUrl + 'docs/' + (language ? language + '/' : '') + doc;
   }
 
   pageUrl(doc, language) {
     const baseUrl = this.props.config.baseUrl;
-    return baseUrl + (language ? `${language}/` : '') + doc;
+    return baseUrl + (language ? language + '/' : '') + doc;
   }
 
   render() {
+    const currentYear = new Date().getFullYear();
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
@@ -36,46 +34,46 @@ class Footer extends React.Component {
             )}
           </a>
           <div>
+
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('intro.html', this.props.language)}>
+              Getting Started
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
+
+            <a href={this.docUrl('groceristar-project.html', this.props.language)}>
+              GroceriStar section
             </a>
-            <a href={this.docUrl('doc3.html', this.props.language)}>
-              API Reference (or other categories)
+
+            <a href={this.docUrl('chickenkyiv-project.html', this.props.language)}>
+              ChickenKyiv section
             </a>
+
+            <a href={this.docUrl('mealcalendar-project.html', this.props.language)}>
+              Meal Calendar section
+            </a>
+
+            <a href={this.docUrl('searchapi-project.html', this.props.language)}>
+              Search API section
+            </a>
+
+            <a href={this.docUrl('searchform-project.html', this.props.language)}>
+              Search Form section
+            </a>
+
           </div>
           <div>
-            <h5>Community</h5>
-            <a href={this.pageUrl('users.html', this.props.language)}>
-              User Showcase
-            </a>
-            <a
-              href="http://stackoverflow.com/questions/tagged/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
-            <a href="https://discordapp.com/">Project Chat</a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noreferrer noopener">
-              Twitter
-            </a>
+
           </div>
           <div>
             <h5>More</h5>
-            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
+          <a href="https://medium.com/groceristar">Blog</a>
+            <a href="https://github.com/GroceriStar/groceristar-fetch">GitHub</a>
             <a
               className="github-button"
               href={this.props.config.repoUrl}
               data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
-              data-show-count="true"
+              data-count-href="GroceriStar/groceristar-fetch/stargazers"
+              data-show-count={true}
               data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub">
               Star
@@ -83,18 +81,18 @@ class Footer extends React.Component {
           </div>
         </section>
 
-        <a
+        {/* <a
           href="https://code.facebook.com/projects/"
           target="_blank"
           rel="noreferrer noopener"
           className="fbOpenSource">
           <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
+            src={this.props.config.baseUrl + 'img/oss_logo.png'}
             alt="Facebook Open Source"
             width="170"
             height="45"
           />
-        </a>
+        </a> */}
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
--- 2website/siteConfig.js
+++ 2website/siteConfig.js
@@ -5,10 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// See https://docusaurus.io/docs/site-config for all the possible
+// See https://docusaurus.io/docs/site-config.html for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
+/* List of projects/orgs using your project for the users page */
 const users = [
   {
     caption: 'User1',
@@ -21,46 +21,45 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'GroceriStar Fetch Module' /* title for your website */,
+  tagline: 'Documentation',
+  //url: 'https://your-docusaurus-test-site.com' /* your website url */,
+  //baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+     url: 'https://groceristar.github.io',
+     baseUrl: '/static-food-data-documentation/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'static-food-data-documentation',
+  organizationName: 'groceristar',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
+     {doc: 'summary', label: 'Summary'},
+       {href : 'https://github.com/GroceriStar/groceristar-fetch/', label: 'GitHub'},
     {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/docusaurus.svg',
-  footerIcon: 'img/docusaurus.svg',
-  favicon: 'img/favicon.png',
+  // https://raw.githubusercontent.com/GroceriStar/creative/master/logo/LOGO1.png
+  headerIcon: 'img/LOGO1.png',
+  footerIcon: 'img/LOGO1.png',
+  favicon: 'img/LOGO1.png',
 
-  /* Colors for website */
+  /* colors for website */
   colors: {
     primaryColor: '#2E8555',
     secondaryColor: '#205C3B',
   },
 
-  /* Custom fonts for website */
-  /*
-  fonts: {
+  /* custom fonts for website */
+  /*fonts: {
     myFont: [
       "Times New Roman",
       "Serif"
@@ -69,38 +68,32 @@ const siteConfig = {
       "-apple-system",
       "system-ui"
     ]
-  },
-  */
+  },*/
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright:
+    'Copyright © ' +
+    new Date().getFullYear() +
+    ' GroceriStar company',
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
+    // Highlight.js theme to use for syntax highlighting in code blocks
     theme: 'default',
   },
 
-  // Add custom scripts here that would be placed in <script> tags.
+  // Add custom scripts here that would be placed in <script> tags
   scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
+  /* On page navigation for the current documentation page */
   onPageNav: 'separate',
-  // No .html extensions for paths.
-  cleanUrl: true,
 
-  // Open Graph and Twitter card images.
+  /* Open Graph and Twitter card images */
   ogImage: 'img/docusaurus.png',
   twitterImage: 'img/docusaurus.png',
 
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
-
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+    repoUrl: 'https://github.com/GroceriStar/groceristar-fetch',
 };
 
 module.exports = siteConfig;
